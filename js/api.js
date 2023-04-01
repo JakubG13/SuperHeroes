4e9acd2eb5c4aef5dca14603d314e775a0847cb3
Jakub Gaj <gaju2001@o2.pl>
2023-04-01 20:56:57 +0200

    catch need info from api

--- js/api.js
+++ js/api.js
@@ -5,8 +5,34 @@ const options = {
     "X-RapidAPI-Host": "superhero-search.p.rapidapi.com",
   },
 };
+// fetch("https://superhero-search.p.rapidapi.com/api/heroes", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+let name = "";
+let img = "";
+let stats = [];
+const generateCard = () => {
+  fetch("https://superhero-search.p.rapidapi.com/api/heroes", options)
+    .then((response) => response.json())
+    .then((response) => response[0])
+    .then((response) => response["images"])
+    .then((response) => (img = response["md"]));
 
-fetch("https://superhero-search.p.rapidapi.com/api/heroes", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+  fetch("https://superhero-search.p.rapidapi.com/api/heroes", options)
+    .then((response) => response.json())
+    .then((response) => response[0])
+    .then((response) => (name = response["name"]));
+
+  fetch("https://superhero-search.p.rapidapi.com/api/heroes", options)
+    .then((response) => response.json())
+    .then((response) => response[0])
+    .then((response) => response["powerstats"])
+    .then((response) => Object.entries(response))
+    .then((response) => {
+      for (let i = 0; i < 6; i++) {
+        let stat = response[i].toString();
+        stats.push(stat);
+      }
+    });
+};
