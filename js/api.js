c4dcb326ba5960a2d81c3754f435ded9bff0f066
Jakub Gaj <gaju2001@o2.pl>
2023-04-01 22:10:57 +0200

    fix bugs

--- js/api.js
+++ js/api.js
@@ -1,38 +1,44 @@
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "62da9f3eeamsh3845601a94a8d35p17648fjsn81e9f988af40",
+    "X-RapidAPI-Key": "f6d6e89aa5mshe5ce7c0d079fd71p11933djsnb5619368c7e1",
     "X-RapidAPI-Host": "superhero-search.p.rapidapi.com",
   },
 };
-// fetch("https://superhero-search.p.rapidapi.com/api/heroes", options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
+
 let name = "";
 let img = "";
 let stats = [];
+const api = "https://superhero-search.p.rapidapi.com/api/heroes";
 const generateCard = () => {
-  fetch("https://superhero-search.p.rapidapi.com/api/heroes", options)
+  fetch(api, options)
     .then((response) => response.json())
     .then((response) => response[0])
     .then((response) => response["images"])
     .then((response) => (img = response["md"]));
 
-  fetch("https://superhero-search.p.rapidapi.com/api/heroes", options)
+  fetch(api, options)
     .then((response) => response.json())
     .then((response) => response[0])
     .then((response) => (name = response["name"]));
 
-  fetch("https://superhero-search.p.rapidapi.com/api/heroes", options)
+  fetch(api, options)
     .then((response) => response.json())
     .then((response) => response[0])
     .then((response) => response["powerstats"])
     .then((response) => Object.entries(response))
     .then((response) => {
+      stats = [];
       for (let i = 0; i < 6; i++) {
         let stat = response[i].toString();
         stats.push(stat);
       }
     });
 };
+
+generateCard();
+
+// fetch("https://superhero-search.p.rapidapi.com/api/heroes", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
